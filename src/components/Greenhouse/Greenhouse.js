cda63786f007f229e9bc1ee394399fb6ae27823f
michellekontoff <mlkontoff@gmail.com>
2021-07-08 13:58:32 -0400

    finished phase 1

--- src/components/Greenhouse/Greenhouse.js
+++ src/components/Greenhouse/Greenhouse.js
@@ -1,5 +1,7 @@
 import dayImage from './images/greenhouse-day.jpg';
 import nightImage from './images/greenhouse-night.jpg';
+import { ThemeProvider, useTheme } from '../../context/ThemeContext'
+import { useEffect } from 'react';
 
 import './Greenhouse.css';
 
@@ -7,9 +9,15 @@ import LightSwitch from './LightSwitch';
 import ClimateStats from './ClimateStats';
 
 function Greenhouse() {
+    const { themeName } = useTheme()
+
+    // useEffect(() => {
+    //     console.log(themeName)
+    // })
+
   return (
     <section>
-      <img className='greenhouse-img' src={dayImage} alt='greenhouse' />
+      <img className='greenhouse-img' src={themeName === 'day' ? dayImage : nightImage} alt='greenhouse' />
       <LightSwitch />
       <ClimateStats />
     </section>
